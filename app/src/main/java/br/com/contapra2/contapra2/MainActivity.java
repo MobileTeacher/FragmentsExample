ccc889ed6694c246a4c98a5ce15e0f1d2b347f5c
hallpaz <hallpaaz@gmail.com>
2018-06-19 01:56:23 -0300

    before class

--- app/src/main/java/br/com/contapra2/contapra2/MainActivity.java
+++ app/src/main/java/br/com/contapra2/contapra2/MainActivity.java
@@ -1,21 +1,77 @@
 package br.com.contapra2.contapra2;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+
+    String currentText = "Fragmento inicial";
+    DrawerLayout drawer;
+    FragmentPagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_drawer_layout);
 
-        TextFragment textFragment = new TextFragment();
+        drawer = findViewById(R.id.drawer);
 
+        changeFragment(currentText);
+
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(Gravity.START)){
+            drawer.closeDrawer(Gravity.START);
+        }
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawer.closeDrawer(Gravity.START);
+        int itemId = item.getItemId();
+        switch (itemId){
+            case R.id.cool_fragment:
+            case R.id.black_fragment:
+            case R.id.lost_fragment:
+                if (!currentText.equals(item.getTitle().toString())){
+                    changeFragment(item.getTitle().toString());
+                }
+                break;
+            default:
+                Toast.makeText(this, "Escolha inválida!", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    private void changeFragment(String message){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.main_fragment, textFragment);
+        TextFragment textFragment = TextFragment.newInstance(message);
+        transaction.replace(R.id.main_fragment, textFragment);
         transaction.commit();
+        currentText = message;
     }
 }
--- app/src/main/java/br/com/contapra2/contapra2/TextFragment.java
+++ app/src/main/java/br/com/contapra2/contapra2/TextFragment.java
@@ -3,9 +3,11 @@ package br.com.contapra2.contapra2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -14,7 +16,7 @@ import android.view.ViewGroup;
 public class TextFragment extends Fragment {
 
 
-    String message = "";
+    private static final String TEXT_VALUE = "text_value";
     public TextFragment() {
         // Required empty public constructor
     }
@@ -24,7 +26,22 @@ public class TextFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_text, container, false);
+        TextView textView = rootView.findViewById(R.id.center_text);
+        if (getArguments() != null){
+            textView.setText(getArguments().getString(TEXT_VALUE, "ERRO"));
+        }
+        return rootView;
+    }
+
+    public static TextFragment newInstance(String mainText) {
+        Bundle args = new Bundle();
+        TextFragment fragment = new TextFragment();
+        args.putString(TEXT_VALUE, mainText);
+        fragment.setArguments(args);
+        Log.d("DEBUG", mainText);
+
+        return fragment;
     }
 
 }
